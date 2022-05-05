4d08948018d16dc4f87a7ecc32f5e2133f23b5ab
ratioooo <pierrick.bauer@reseau.eseo.fr>
2022-05-05 15:09:57 +0200

    gros caca

---
+++ 90°.py
@@ -0,0 +1,55 @@
+from matplotlib.pyplot import *
+from math import *
+
+title("Clothoïde")
+x1 = [0, 1.00, 1.01]
+y1 = [0, 0, 0]
+x2=[0,-1.00,-1.01]
+y2=[0,0,0]
+
+
+
+
+
+L = 1.00
+
+C=int(input("quelle est al vitesse?"))
+
+phi = 0
+i = 2
+n=int(input("nombre?"))
+"""while i<n :
+
+
+    L = L + C
+    phi = phi + L / C ** 2
+    x1.append(x1[i] + cos(phi) * C)
+    y1.append(y1[i] + sin(phi) * C)
+    i=i+1"""
+L=1.0
+
+phi = 0
+i = 2
+while x2[i]<x2[i-1] :
+
+
+    L = L + C
+    phi = phi + L / C ** 2
+    x2.append(x2[i] - cos(phi) * C)
+    y2.append(y2[i] - sin(phi) * C)
+    i=i+1
+
+
+
+
+
+x2.reverse()
+y2.reverse()
+
+"""for k in range(len (x1)):
+    x2.append(x1[k])
+    y2.append(y1[k])"""
+plot(x2, y2, '-0', color='red')
+
+
+show()
