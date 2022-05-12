c3f615c285de97672742cfa45a4242a4d8835759
Coussinman <nathanael.boudier@gmail.com>
2022-05-12 15:30:37 +0200

    kbg

--- nath.py
+++ nath.py
@@ -28,12 +28,25 @@ def dvpt_limite_de_y(s, nmax):
 def A_angot(s, nmax):
     a = 0
     for n in range(0, nmax):
-        a += ((-1) ** (n+1)) * (1.5*(4*n+1))/((pi*s**2)**(2*n+1))
+        a += ((-1) ** (n + 1)) * (1.5 * (4 * n + 1)) / ((pi * s ** 2) ** (2 * n + 1))
     return a
 
 
-def B_angot(s , )
+def B_angot(s, nmax):
+    b = 0
+    for n in range(0, nmax):
+        b += ((-1) ** (n + 1)) * (1.5 * (4 * n - 1)) / ((pi * s ** 2) ** (2 * n))
+    return b
 
+
+def x_par_angot(s, nmax):
+    return (1 / 2 + A_angot(s, nmax) * (cos((pi * s ** 2) / 2) / (pi * s)) - B_angot(s, nmax) * (
+            (sin((pi * s ** 2) / 2)) / (pi * s)))
+
+
+def y_par_angot(s, nmax):
+    return (1 / 2 + A_angot(s, nmax) * (sin((pi * s ** 2) / 2) / (pi * s)) - B_angot(s, nmax) * (
+            (cos((pi * s ** 2) / 2)) / (pi * s)))
 
 
 ω = 10
@@ -46,8 +59,10 @@ precision_du_devpt_limité = 300
 A = 1
 
 for i in range(len(w)):
-    liste_x1.append(dvpt_limite_de_x(w[i], precision_du_devpt_limité))
-    liste_y.append(dvpt_limite_de_y(w[i], precision_du_devpt_limité))
+    '''liste_x1.append(dvpt_limite_de_x(w[i], precision_du_devpt_limité))
+    liste_y.append(dvpt_limite_de_y(w[i], precision_du_devpt_limité))'''
+    liste_x1.append(x_par_angot(w[i], precision_du_devpt_limité))
+    liste_y.append(y_par_angot(w[i], precision_du_devpt_limité))
 
 print(liste_x1)
 print(liste_y)
