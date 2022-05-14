1dd0709056a0271d642d3aa37c67b67126dfd506
Timothee20V <timothee.viardtv@gmail.com>
2022-05-14 16:09:21 +0200

    Create test

--- Vecteurs.py
+++ Vecteurs.py
@@ -9,40 +9,34 @@ class Vect:
         self.y2 = y2
         self.x = x2 - x1
         self.y = y2 - y1
+        self.norme = sqrt((self.x ** 2 + self.y ** 2))
         self.genre = genre
 
     def affiche(self):
         print(self.x, self.y, self.genre)
 
-    def __sub__(self, other):
-        if self.genre == "vecteur" and other.genre == "vecteur":
-            return Vect(self.x - other.x, self.y - other.y, genre="vecteur")
+    def __mul__(self, other):
+        if self.genre == "vecteur":
+            return Vect(self.x1 * other, self.y1 * other, self.x2 * other, self.y2 * other, genre="vecteur")
 
     def produit_scalaire(self, other):
         val = self.x * other.x + self.y * other.y
         return val
 
-    def norme(self):
-        if self.genre == "vecteur":
-            val = sqrt((self.x ** 2 + self.y ** 2))
-            return val
-
     def angle(self, other):
         if (self.genre and other.genre) == 'vecteur':
             scalaire = self.produit_scalaire(other)
-            BA = self.norme()
-            BC = other.norme()
+            BA = self.norme
+            BC = other.norme
             print(BA, BC)
             angle_rad = acos(scalaire / (BA * BC))
             angle_deg = 360 * angle_rad / (2 * pi)
             return angle_deg
 
 
-AB = Vect(0,0,5,5,'vecteur')
-CB = Vect(0,0,-5,5,'vecteur')
+AB = Vect(0, 0, 5, 5, 'vecteur')
+CB = Vect(0, 0, -5, 5, 'vecteur')
 print(AB.x, AB.y)
 print(CB.x, CB.y)
-print(AB.produit_scalaire(CB))
-print(AB.angle(CB))
-
-
+print(AB.norme)
+print((AB * 2).norme)
--- test.py
+++ test.py
@@ -27,54 +27,58 @@ def clear():
     points_y.clear()
 
 
-def bissectrice(BA ,BC):
+def bissectrice(BA, BC):
     xA, yA = BA.x2, BA.y2
     xB, yB = BA.x1, BA.y1
     xC, yC = BC.x2, BC.y2
-    BD = Vect(BC.x1, BC.y1, BC.x1+100, BC.y1, 'vecteur')
+    BD = Vect(BC.x1, BC.y1, BC.x1 + 100, BC.y1, 'vecteur')
     angle_1 = BD.angle(BA)
     angle_2 = BA.angle(BC) / 2
+    T = R/tan(BA.angle(BC)/2)
+    U = R / sin(((2 * pi) * (BA.angle(BC)) / 360) / 2)
+
+    print('T:', T, 'U:', U)
 
     # meme moitié
     if xA >= xB and xC >= xB and yA >= yB >= yC:
         print("1")
         angleF = angle_1 - angle_2
-        xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+        xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
 
     if xA >= xB and xC >= xB and yA <= yB <= yC:
         print("2")
         angleF = angle_1 - angle_2
-        xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+        xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
 
     if xA <= xB and xC <= xB and yA <= yB <= yC:
         print("3")
         angleF = angle_1 + angle_2
-        xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+        xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
 
     if xA <= xB and xC <= xB and yA >= yB >= yC:
         print("4")
         angleF = angle_1 + angle_2
-        xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+        xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
 
     if xA <= xB <= xC and yA <= yB and yC <= yB:
         print("5")
         angleF = angle_1 - angle_2
-        xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+        xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
 
     if xA >= xB >= xC and yA <= yB and yC <= yB:
         print("6")
         angleF = angle_1 + angle_2
-        xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+        xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
 
     if xA <= xB <= xC and yA >= yB and yC >= yB:
         print("7")
         angleF = angle_1 - angle_2
-        xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+        xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
 
     if xA >= xB >= xC and yA >= yB and yC >= yB:
         print("8")
         angleF = angle_1 + angle_2
-        xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+        xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
 
     # symétrique par rapport au centre
     if xA >= xB >= xC and yA > yB > yC:
@@ -82,95 +86,91 @@ def bissectrice(BA ,BC):
         if BD.angle(BC) + BD.angle(BA) > 180:
             print("9.1")
             angleF = angle_1 + angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
         else:
             print("9.2")
             angleF = angle_1 - angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
 
     if xA <= xB <= xC and yA < yB < yC:
         print("10")
         if BD.angle(BC) + BD.angle(BA) > 180:
             print("10.1")
             angleF = angle_1 + angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
         else:
             print("10.2")
             angleF = angle_1 - angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
 
     if xA <= xB <= xC and yA > yB > yC:
         print("11")
         if BD.angle(BC) + BD.angle(BA) > 180:
             print("11.1")
             angleF = angle_1 + angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
         else:
             print("11.2")
             angleF = angle_1 - angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
 
     if xA >= xB >= xC and yA < yB < yC:
         print("12")
         if BD.angle(BC) + BD.angle(BA) > 180:
             print("12.1")
             angleF = angle_1 + angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
         else:
             print("12.2")
             angleF = angle_1 - angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
 
     # meme quart
     if xA > xB and xC > xB and yA > yB and yC > yB:
         print("13")
-        if yA < yC:
+        if BD.angle(BA) - BD.angle(BC) < 0:
             print("13.1")
             angleF = angle_1 + angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
         else:
             print("13.2")
             angleF = angle_1 - angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
 
     if xA < xB and xC < xB and yA > yB and yC > yB:
         print("14")
-        if yA < yC:
+        if BD.angle(BA) - BD.angle(BC) > 0:
             print("14.1")
             angleF = angle_1 - angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
         else:
             print("14.2")
             angleF = angle_1 + angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB + sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB + sin(angleF * 2 * pi / 360) * U
 
     if xA < xB and xC < xB and yA < yB and yC < yB:
         print("15")
-        if yA > yC:
+        if BD.angle(BA) - BD.angle(BC) > 0:
             print("15.1")
             angleF = angle_1 - angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
         else:
             print("15.2")
             angleF = angle_1 + angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
 
     if xA > xB and xC > xB and yA < yB and yC < yB:
         print("16")
-        if yA > yC:
+        if BD.angle(BA) - BD.angle(BC) < 0:
             print("16.1")
             angleF = angle_1 + angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
         else:
             print("16.2")
             angleF = angle_1 - angle_2
-            xE, yE = xB + cos(angleF * 2 * pi / 360) * R, yB - sin(angleF * 2 * pi / 360) * R
+            xE, yE = xB + cos(angleF * 2 * pi / 360) * U, yB - sin(angleF * 2 * pi / 360) * U
 
-
-    print("DBA:", angle_1)
-    print("ABC:", angle_2 * 2)
-    print("ABC/2:", angle_2)
-    print("F:", angleF)
+    print('T = ', BA*(1/(BA.norme)))
     BE = Vect(xB, yB, xE, yE, 'vecteur')
     cnv.create_text(xE, yE, text='●')
     cnv.create_text(xE, yE + 10, text=round(BE.angle(BA)), fill='green')
