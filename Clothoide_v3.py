9993d617eefdf4c12aeb5642701a2a0ba2147bfc
Timothee20V <timothee.viardtv@gmail.com>
2022-06-18 17:24:45 +0200

    V2

--- Clothoide_v3.py
+++ Clothoide_v3.py
@@ -21,7 +21,6 @@ def clothoide_fct(xa,ya,xb,yb,xf,yf,xe,ye):
     phi=0
     x2=[xa,xf]
     y2=[ya,yf]
-
     #la liste dees points
 
 
@@ -31,7 +30,7 @@ def clothoide_fct(xa,ya,xb,yb,xf,yf,xe,ye):
 
 
     i = 1
-    C=L
+    C=1
     #on itère une fois le programme pour éviter la division par 0
     L = L + C
     phi = phi + L / C ** 2
@@ -40,8 +39,7 @@ def clothoide_fct(xa,ya,xb,yb,xf,yf,xe,ye):
     i = i + 1
     if xb!=xe:
         #xb=xe entraine un division par 0
-        """while ((x2[-1]-xe)*(yb-ye)/abs(xb-xe)+y2[-1])*((x2[-2]-xe)*(yb-ye)/abs(xb-xe)+y2[-2])>0:"""
-        while len (y2)<100:
+        while ((x2[-1]-xe)*(yb-ye)/abs(xb-xe)+y2[-1])*((x2[-2]-xe)*(yb-ye)/abs(xb-xe)+y2[-2])>0:
 #on vérifie que ce pan de droite ne traverse pas BE
 
             L = L + C
@@ -49,7 +47,7 @@ def clothoide_fct(xa,ya,xb,yb,xf,yf,xe,ye):
             x2.append(x2[i] + cos(phi) * C*x1x+sin(phi)*C*y1x)
             y2.append(y2[i] + sin(phi) * C*y1y+cos(phi)*C*x1y)
             i=i+1
-    elif xb==xe:
+    if xb==xe:
         while x2[-1]*x2[-2]>0:
             # on vérifie que ce pan de droite ne traverse pas BE
 
@@ -58,14 +56,11 @@ def clothoide_fct(xa,ya,xb,yb,xf,yf,xe,ye):
             x2.append(x2[i] + cos(phi) * C * x1x + sin(phi) * C * y1x)
             y2.append(y2[i] + sin(phi) * C * y1y + cos(phi) * C * x1y)
             i = i + 1
-
-
-
-    """#la symétrie
+#la symétrie
     l=len(x2)
     for  i in range(1,l-2):
         x2.append(2*xe-x2[l-i])
-        y2.append(2*ye-y2[l-i])"""
+        y2.append(2*ye-y2[l-i])
     plt.plot(x2, y2, '-0', color='red')
 
     plt.show()
